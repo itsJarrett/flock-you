8e8623ff22e949ad25f7c032756b6132ff5c4417
itsJarrett <jarrettboice@outlook.com>
2026-01-09 19:59:35 -0500

    chore: Update versionCode to 1.1 in build.gradle.kts

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "github.itsjarrett.flockyou"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
